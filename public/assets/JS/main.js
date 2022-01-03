71d2b79b2624f65800c0e70666c08d1195c75797
Fabricio Narvaez <narvaezfabricio38@gmail.com>
2022-01-03 13:07:01 +0100

    Menú de navegación adaptable

---
+++ public/assets/JS/main.js
@@ -0,0 +1,30 @@
+var abrir = "";
+var menu = "";
+var cerrado = true;
+
+function initVar(){
+    abrir = document.getElementById("open");
+    menu = document.getElementById("menu");
+
+    abrir.addEventListener('click', ()=>{
+        //console.log(abrir);
+        if(cerrado){
+            menu.style.width = '100%';
+            cerrado = false;
+            abrir.innerHTML = "&#10005";
+        }else{
+            menu.style.width = '0%';
+            menu.style.overflow = 'hidden';
+            abrir.innerHTML = "&#9776";
+            cerrado = true;
+        }
+    });
+
+    window.addEventListener('resize', ()=>{
+        if(screen.width > 451){
+            menu.style.removeProperty('overflow');
+            menu.style.removeProperty('width');
+        }
+    })
+}
+
